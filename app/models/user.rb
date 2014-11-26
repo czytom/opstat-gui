62e7235f7145d10ee1eac05990acbc225b1df8cf
Krzysztof Tomczyk <czytom@gmail.com>
2014-11-26 23:00:02 +0100

    disable storing plain text password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User
          :timeoutable, :validatable
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  key :password, String
   key :encrypted_password, String
   key :username, String
   key :email, String
